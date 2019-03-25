4da130215a44933b1d731e43c1bbaa426f5e391e
Iskra B <ibatistapoblete@gmail.com>
2019-03-25 12:32:40 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,8 +6,8 @@ def array_with_two_elements
   ["1", "2"]
 end
 
-def first_element
-  array.first
+def first_element(array)
+  [0]
 end
 
 def third_element(array)
@@ -16,4 +16,13 @@ end
 
 def last_element(array)
   array.last
+end
+
+def first_element_with_array_methods
+end
+
+def last_element_with_array_methods
+end
+
+def length_of_array
 end
