5dd9c7e19e150657f1874ca4b12d74e680f2e395
Iskra B <ibatistapoblete@gmail.com>
2019-03-25 12:01:03 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,12 @@
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  ["1", "2"]
+end
+
+def first_element (@taylor_swift)
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  @taylor_swift.first
+end
